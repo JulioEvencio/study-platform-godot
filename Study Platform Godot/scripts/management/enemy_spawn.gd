fd9bb942ac03d198d4475da160f5b2cbc991998e
JulioEvencio <julioigreja.dev@gmail.com>
2024-04-20 20:12:13 -0300

    update: fix bug - enemu amount max for 1

--- Study Platform Godot/scripts/management/enemy_spawn.gd
+++ Study Platform Godot/scripts/management/enemy_spawn.gd
@@ -7,20 +7,24 @@ class_name EnemySpawn
 
 var whale_scene : PackedScene = preload("res://scenes/enemies/whale.tscn")
 
+var enemy_amount : int = 0
 var can_spawn : bool = true
 
 func _ready() -> void:
 	spawn_enemy()
 
 func timer_start() -> void:
+	enemy_amount -= 1
 	timer.start()
 
 func spawn_enemy() -> void:
-	var whale : Whale = whale_scene.instantiate()
-	whale.level = level_scene
-	whale.position = position
-	whale.connect("dead", timer_start)
-	level_scene.add_entities(whale)
+	if enemy_amount < 1:
+		var whale : Whale = whale_scene.instantiate()
+		whale.level = level_scene
+		whale.position = position
+		whale.connect("dead", timer_start)
+		level_scene.add_entities(whale)
+		enemy_amount += 1
 
 func _on_timer_timeout() -> void:
 	if can_spawn:
